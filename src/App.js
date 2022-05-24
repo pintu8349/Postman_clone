8e38a220f9a24c3b55516ed0f8cd9145cd2498a7
Tushar <Tusharsolanki1011@gmail.com>
2022-05-24 19:26:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+  
+
+
+  return (
+   
+      <div className="App">
+      <h1>Hello </h1>
+      <Home />
+      </div>
+  
+
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+const Home = ()=> {
+    return (
+        <p>Hello form</p>
+    )
+
+}
+export default Home;

4fdebc2374572d51869716d274f9c6a1b9fc0557
Tushar <Tusharsolanki1011@gmail.com>
2022-05-29 01:58:49 +0530

    compleated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useContext ,useState } from "react";
+import { useContext, useState } from "react";
 import Form from "./Form";
 import Header from "./Header";
 import { Box } from "@mui/material";
@@ -6,44 +6,52 @@ import { makeStyles } from "@mui/styles";
 import SelectTab from "./SelectTab";
 import Response from "./Response";
 import ErrorScreen from "./ErrorScreen";
-import SnackBar from "./SnackBar"
-import {DataContext} from '../context/Dataprovider';
-import {getData} from '../service/api'
+import SnackBar from "./SnackBar";
+import { DataContext } from "../context/Dataprovider";
+import { getData } from "../service/api";
 
-import {checkParams} from '../utils/common-utils'
+import { checkParams } from "../utils/common-utils";
 const useStyles = makeStyles({
-    component: {
-        width :'60%',
-        margin: '20px auto 0 auto'
-    }
-})
+  component: {
+    width: "60%",
+    margin: "20px auto 0 auto",
+  },
+});
 //home started building
-const Home = ()=> {
-    const classes = useStyles();
-    const [error,setError] =useState(false);
-    const [errorMsg,setErrorMsg]=useState('');
-    const {formData ,jsonText,paramData,headerData} =useContext(DataContext);
-    const onSendClick =async () => {
-        if(!checkParams(formData ,jsonText,paramData,headerData,setErrorMsg)){
-            setError(true);
-            return false;
-        }
-        let response= await getData(formData ,jsonText,paramData,headerData);
+const Home = () => {
+  const classes = useStyles();
+  const [error, setError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+  const [errorResponse, setErrorResponse] = useState(false);
+  const [apiResponse, setApiResponse] = useState({});
+
+  const { formData, jsonText, paramData, headerData } = useContext(DataContext);
+  const onSendClick = async () => {
+    if (!checkParams(formData, jsonText, paramData, headerData, setErrorMsg)) {
+      setError(true);
+      return false;
     }
-    return (
-        <>
-        <Header />
-        <Box className={classes.component}>
-            <Form onSendClick={onSendClick}/>
-            <SelectTab />
-            {/* <Response /> */}
-            <ErrorScreen />
-            {error && <SnackBar error={error} setError={setError} errorMsg={errorMsg}  />}
-        </Box>
-       
-        </>
+    let response = await getData(formData, jsonText, paramData, headerData);
+    if (response === "error") {
+      setErrorResponse(true);
+      return;
+    }
+    setErrorResponse(false);
+    setApiResponse(response.data);
+  };
+  return (
+    <>
+      <Header />
+      <Box className={classes.component}>
+        <Form onSendClick={onSendClick} />
+        <SelectTab />
 
-    )
-
-}
+        {errorResponse ? <ErrorScreen /> : <Response data={apiResponse}/>}
+        {error && (
+          <SnackBar error={error} setError={setError} errorMsg={errorMsg} />
+        )}
+      </Box>
+    </>
+  );
+};
 export default Home;
--- src/components/Response.jsx
+++ src/components/Response.jsx
@@ -1,22 +1,42 @@
 import { Box, Typography, TextareaAutosize } from "@mui/material";
-const textareaStyle = { 
-    width: '100%', 
-    padding: 10,  
-    background: `url(http://i.imgur.com/2cOaJ.png)`,
-    backgroundAttachment: 'local',
-    backgroundRepeat: 'no-repeat',
-    paddingLeft: 35,
-    paddingTop: 10,
-    borderColor: '#ccc' 
-}
+const textareaStyle = {
+  width: "100%",
+  padding: 10,
+  background: `url(http://i.imgur.com/2cOaJ.png)`,
+  backgroundAttachment: "local",
+  backgroundRepeat: "no-repeat",
+  paddingLeft: 35,
+  paddingTop: 10,
+  borderColor: "#ccc",
+};
 
-const Response = () => {
+const Response = ({ data }) => {
+  let obj = data;
+
+  let readableobj = "{ \n";
+  for (let key in obj) {
+    readableobj += "\t";
+    readableobj +=
+      typeof obj[key] === "string"
+        ? `${key}: "${obj[key]}"`
+        : `${key}: ${obj[key]}`;
+    if (Object.keys(obj).pop() !== key.toString()) {
+      readableobj += ",\n";
+    }
+  }
+  readableobj += "\n}";
   return (
     <Box>
       <Typography mt={2} mb={2}>
-        JSOn
+        Response
       </Typography>
-      <TextareaAutosize minRows={3} maxRows={5} style={textareaStyle} disabled={'disabled'} />
+      <TextareaAutosize
+        minRows={3}
+        maxRows={5}
+        style={textareaStyle}
+        disabled="disabled"
+        value={readableobj}
+      />
     </Box>
   );
 };
